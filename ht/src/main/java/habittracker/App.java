9c817750e55f55954b0a63d975818d1228441e83
BorintJohn <71138323+BorintJohn@users.noreply.github.com>
2023-04-21 01:24:44 +0700

    Add files via upload

---
+++ ht/src/main/java/habittracker/App.java
@@ -0,0 +1,22 @@
+package habittracker;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+        System.out.println( "Hello World!" );
+        //excelManager.inserttest();
+        //excelManager.createHabitStorage();
+        //excelManager.inserthabit("Study", true, false, false, true, true,true, true, true, true, true);
+        
+        //excelManager.deletehabit(1,false);
+        //excelManager.readhabit();
+        
+    }
+}
